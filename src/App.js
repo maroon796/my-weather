80a2a1c884c7a6a38243a6c89f07dafdcd56895f
Grygory Ksheminsky <maroon785@gmail.com>
2023-02-11 01:51:57 +0200

    Working with useState. Adding Card

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import Header from './components/Header/Header';
 import classes from './App.module.css';
 import SearchBar from './components/SearchBar/SearchBar';
+import Card from './components/Card/Card';
 
 function App() {
   const [weatherInfo, setWeatherInfo] = useState({});
+  const [activateWeather, setActivateWeather] = useState(false);
+  const [error, setError] = useState(null);
+
   const getWeatherInfo = () => {};
   return (
     <>
       <Header />
       <main className={classes.main}>
         <SearchBar getWeatherInfo={getWeatherInfo} />
+
+        <Card weather={weatherInfo} activate={activateWeather} isError={error} />
       </main>
     </>
   );
