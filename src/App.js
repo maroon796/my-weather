1dbe0de2689bdd9204b14d9320b16024c2db922d
Grygory Ksheminsky <maroon785@gmail.com>
2023-02-11 01:21:13 +0200

    Adding Header on the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Header from './components/Header/Header';
+import classes from './App.module.css';
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
