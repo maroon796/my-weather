62f730e8f0b211bc9e0bfae43fcaadaf69b063f9
Grygory Ksheminsky <maroon785@gmail.com>
2023-02-11 14:05:18 +0200

    Importing icons to Card component,working with css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,23 @@
-import React from 'react';
+import WeatherDetails from './WeatherDetails';
+import classes from './Card.module.css';
+import { PreviewIcon } from '../../assets/icons/PreviewIcon';
+import { Clear } from '../../assets/icons/Clear';
+import { CloudWind } from '../../assets/icons/CloudWind';
+import { Clouds } from '../../assets/icons/Clouds';
+import { Snow } from '../../assets/icons/Snow';
+import { Rain } from '../../assets/icons/Rain';
+import { Drizzle } from '../../assets/icons/Drizzle';
+import { Thunderstorm } from '../../assets/icons/Thunderstorm';
+
+const currentWeather = {
+  Clear: <Clear />,
+  Clouds: <Clouds />,
+  Rain: <Rain />,
+  'Cloud-Wind': <CloudWind />,
+  Drizzle: <Drizzle />,
+  Snow: <Snow />,
+  Thunderstorm: <Thunderstorm />,
+};
 
 const Card = () => {
   return <div>Card</div>;
