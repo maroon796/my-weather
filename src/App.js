a4258dd88577aa9974fef983445a62c44278a5dd
Grygory Ksheminsky <maroon785@gmail.com>
2023-02-11 02:51:18 +0200

    Fetching weather information from API

--- src/App.js
+++ src/App.js
@@ -9,7 +9,62 @@ function App() {
   const [activateWeather, setActivateWeather] = useState(false);
   const [error, setError] = useState(null);
 
-  const getWeatherInfo = () => {};
+  const getWeatherInfo = (cityName) => {
+    setError(false);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ba10861a04d82b33b3fefc64fe028d49`,
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        const weekday = [
+          'Sunday',
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+        ];
+
+        const date = new Date();
+        const dayOfWeek = weekday[date.getDay()];
+
+        const monthNames = [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December',
+        ];
+
+        const currentMonth = monthNames[date.getMonth()];
+
+        const dayOfMonth = date.getDate();
+
+        setWeatherInfo({
+          description: data.weather[0].main,
+          temperature: Math.round(data.main.temp - 273),
+          date: {
+            dayOfWeek: dayOfWeek,
+            month: currentMonth,
+            dayOfMonth: dayOfMonth,
+          },
+        });
+        setActivateWeather(true);
+      })
+      .catch((err) => {
+        setError(true);
+      });
+  };
   return (
     <>
       <Header />
